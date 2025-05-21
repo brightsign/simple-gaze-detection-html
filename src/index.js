a489faa1b987e626ee3905ac59bc265ff4a60ee7
Greg Herlein <gherlein@herlein.com>
2025-05-21 09:23:56 -0700

    fix-to-horizontal

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const fetchInterval = imageUpdateInterval / oversampling_rate;
 const udpPort = 5002;
 const udpServer = dgram.createSocket('udp4');
 
+// Variables to store the latest values for each variable
 let total_faces = 'N/A';
 let attending_faces = 'N/A';
 
@@ -25,8 +26,8 @@ EDIT BELOW FOR PRESENTATION CHANGES
 --------------------------------------------------------------------------------------------------------------------------------
 */
 
-const image_location_top = 76;  // up-down after rotation
-const image_location_left = 83; // left-right after rotation
+const image_location_top = 70;
+const image_location_left = 90;
 const vidPath = '/GG-horizontal-HD.mp4'
 
 /*
@@ -36,123 +37,123 @@ EDIT ABOVE FOR PRESENTATION CHANGES
 */
 
 function main() {
-  console.log('In Main - Remote Liftoff!');
-  // Fetch the image fetchInterval times per second
-  setInterval(fetchImage, fetchInterval);
+    console.log('In Main - Remote Liftoff!');
+    // Fetch the image fetchInterval times per second
+    setInterval(fetchImage, fetchInterval);
 
-  // Set the video zone src to the VideoPath
-  const videoZone = document.getElementById('video');
-  videoZone.src = vidPath;
+    // Set the video zone src to the VideoPath
+    const videoZone = document.getElementById('video');
+    videoZone.src = vidPath;
 
-  // Set the image location on the screen
-  const imageContainer = document.getElementById('image-container');
-  imageContainer.style.left = image_location_left + '%';
-  imageContainer.style.top = image_location_top + '%';
+    // Set the image location on the screen
+    const imageContainer = document.getElementById('image-container');
+    imageContainer.style.left = image_location_left + '%';
+    imageContainer.style.top = image_location_top + '%';
 
-  // Bind the UDP server to the specified port
-  udpServer.bind(udpPort, () => {
-    console.log(`UDP server listening on port ${udpPort}`);
-  });
+    // Bind the UDP server to the specified port
+    udpServer.bind(udpPort, () => {
+        console.log(`UDP server listening on port ${udpPort}`);
+    });
 
-  // Listen for UDP messages
-  udpServer.on('message', (msg, rinfo) => {
-    // Parse the incoming message
-    handleUdpMessage(msg);
-  });
+    // Listen for UDP messages
+    udpServer.on('message', (msg, rinfo) => {
+        // Parse the incoming message
+        handleUdpMessage(msg);
+    });
 }
 
 window.onload = main;
 
 // Functions
 function fetchImage() {
-  fs.stat(imagePath, (err, stats) => {
-    if (err) {
-      console.log('Error reading image file:', err);
-      return;
-    }
-
-    if (stats.mtimeMs > lastImageUpdateTime) {
-      lastImageUpdateTime = stats.mtimeMs;
-      fs.readFile(imagePath, (err, data) => {
+    fs.stat(imagePath, (err, stats) => {
         if (err) {
-          console.log('Error reading image file:', err);
-          return;
+            console.log('Error reading image file:', err);
+            return;
         }
 
-        const base64Image = `data:image/jpeg;base64,${data.toString('base64')}`;
-        const tempImage = new Image();
-        tempImage.onload = () => {
-          imageElement.src = base64Image;
-          imageElement.style.display = 'block';
-        };
-        tempImage.src = base64Image;
-      });
-    } else if (Date.now() - lastImageUpdateTime > timeout) {
-      console.log('No image update in the last 5 seconds');
-      lastImageUpdateTime = Date.now(); // reset
-      imageElement.src = 'none';
-      imageElement.style.display = 'none';
-    }
-  });
+        if (stats.mtimeMs > lastImageUpdateTime) {
+            lastImageUpdateTime = stats.mtimeMs;
+            fs.readFile(imagePath, (err, data) => {
+                if (err) {
+                    console.log('Error reading image file:', err);
+                    return;
+                }
+
+                const base64Image = `data:image/jpeg;base64,${data.toString('base64')}`;
+                const tempImage = new Image();
+                tempImage.onload = () => {
+                    imageElement.src = base64Image;
+                    imageElement.style.display = 'block';
+                };
+                tempImage.src = base64Image;
+            });
+        } else if (Date.now() - lastImageUpdateTime > timeout) {
+            console.log('No image update in the last 5 seconds');
+            lastImageUpdateTime = Date.now(); // reset
+            imageElement.src = 'none';
+            imageElement.style.display = 'none';
+        }
+    });
 }
 
 function handleUdpMessage(msg) {
-  const message = msg.toString();
-  console.log(`Received message: ${message}`);
-  // Update the corresponding variable
+    const message = msg.toString();
+    console.log(`Received message: ${message}`);
+    // Update the corresponding variable
 
-  if (udpPort === 5000) {
-    const [variable, value] = message.split(':');
-    if (variable === 'session_last_0s') {
-      sessionLast0s = value;
-    } else if (variable === 'session_last_30s') {
-      sessionLast30s = value;
-    } else if (variable === 'session_last_5m') {
-      sessionLast5m = value;
+    if (udpPort === 5000) {
+        const [variable, value] = message.split(':');
+        if (variable === 'session_last_0s') {
+            sessionLast0s = value;
+        } else if (variable === 'session_last_30s') {
+            sessionLast30s = value;
+        } else if (variable === 'session_last_5m') {
+            sessionLast5m = value;
+        }
+    } else if (udpPort === 5001) {
+        // Parse the JSON message
+        const data = JSON.parse(message);
+        // Validate the structure
+        if (
+            typeof data === 'object' &&
+            'all_sessions_count' in data &&
+            'sessions_last_0s' in data &&
+            'sessions_last_30s' in data &&
+            'sessions_last_5m' in data
+        ) {
+            // Update the variables
+            sessionLast0s = data.sessions_last_0s;
+            sessionLast30s = data.sessions_last_30s;
+            sessionLast5m = data.sessions_last_5m;
+        } else {
+            console.log('Invalid JSON structure.');
+        }
+    } else if (udpPort === 5002) {
+        // Parse the JSON message
+        const data = JSON.parse(message);
+        console.log(data);
+        // Validate the structure
+        if (
+            typeof data === 'object' &&
+            'faces_in_frame_total' in data &&
+            'faces_attending' in data
+        ) {
+            // Update the variables
+            total_faces = data.faces_in_frame_total;
+            attending_faces = data.faces_attending;
+        } else {
+            console.log('Invalid JSON structure.');
+        }
     }
-  } else if (udpPort === 5001) {
-    // Parse the JSON message
-    const data = JSON.parse(message);
-    // Validate the structure
-    if (
-      typeof data === 'object' &&
-      'all_sessions_count' in data &&
-      'sessions_last_0s' in data &&
-      'sessions_last_30s' in data &&
-      'sessions_last_5m' in data
-    ) {
-      // Update the variables
-      sessionLast0s = data.sessions_last_0s;
-      sessionLast30s = data.sessions_last_30s;
-      sessionLast5m = data.sessions_last_5m;
-    } else {
-      console.log('Invalid JSON structure.');
-    }
-  } else if (udpPort === 5002) {
-    // Parse the JSON message
-    const data = JSON.parse(message);
-    console.log(data);
-    // Validate the structure
-    if (
-      typeof data === 'object' &&
-      'faces_in_frame_total' in data &&
-      'faces_attending' in data
-    ) {
-      // Update the variables
-      total_faces = data.faces_in_frame_total;
-      attending_faces = data.faces_attending;
-    } else {
-      console.log('Invalid JSON structure.');
-    }
-  }
 
-  // Update the banner with the new values
-  updateBanner();
+    // Update the banner with the new values
+    updateBanner();
 }
 
 // Function to update the displayed values
 function updateBanner() {
-  udpMessageElement.textContent = `
+    udpMessageElement.textContent = `
     ${attending_faces} out of ${total_faces} faces are watching the session.
   `;
 }
